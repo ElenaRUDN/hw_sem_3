37a35b1ce2570ac11399b1910f7149fffe6cc2bf
ElenaRUDN <126961213+ElenaRUDN@users.noreply.github.com>
2023-09-24 18:41:24 +0300

    final version

--- ex1_hw1.cpp
+++ ex1_hw1.cpp
@@ -9,31 +9,32 @@
 #include <string>
 #include <iostream>
 //using namespace std;
-int lena;
+
 
 int main() {
-	std::string stroka;
+	std::string stroka, stroka1;
 	char help;
+	int kolvo = 0;
+	int lena, lena1;
     std::cout << "Write text : " << std::endl;
 	std::getline(std::cin, stroka);
 	std::cout << stroka << std::endl;
 	lena = stroka.length();
-	for (int i = 0; i < lena; ++i) {
-		for (int j = 0; j < lena - 1; ++j) {
-			if (stroka[j] > stroka[j + 1]) {
-				help = stroka[j];
-				stroka[j] = stroka[j + 1];
-				stroka[j + 1] = help;
+	for (int i = 0; i < lena; i++) {
+		if (isalpha(stroka[i]) or isdigit(stroka[i])) {
+			stroka1 += stroka[i];
+		}
+	}
+	lena1 = stroka1.length();
+	for (int i = 0; i < lena1; ++i) {
+		for (int j = 0; j < lena1 - 1; ++j) {
+			if (stroka1[j] > stroka1[j + 1]) {
+				help = stroka1[j];
+				stroka1[j] = stroka1[j + 1];
+				stroka1[j + 1] = help;
 			}
 		}
 	}
-	for (int i = 0; i < lena; i++) {
-		if (isalpha(stroka[i]) == false) {
-			stroka.erase(i, 2);
-			lena = lena - 2;
-			std::cout << stroka << std::endl;
-		}
-	}
-	std::cout << "We made this: " << stroka << std::endl;
+	std::cout << "We made this: " << stroka1 << std::endl;
 	return 0;
 }
