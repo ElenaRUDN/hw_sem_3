9b1358dfdc33265036329e12a51efe4bc5a94a73
ElenaRUDN <126961213+ElenaRUDN@users.noreply.github.com>
2023-11-19 23:37:20 +0300

    first

---
+++ mkp3sem/mkplab1Yakovleva.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <cmath>
+double posled_iter(int y2);
+double grafic(int E);
+float e = 0.0068;
+float M = 0.87;
+
+int main()
+{
+    for (int E = 0; E < 1000; E++) {
+        grafic(E);
+        posled_iter(E);
+        
+    }
+    std::cout << posled_iter(999) << std::endl;
+
+    return 0;
+}
+
+double grafic(int E) {
+    float y1;
+    y1 = E;
+    return y1;
+}
+double posled_iter(int y2) {
+    if (y2 > M) {
+        return e * sin(posled_iter(y2 - (pow(10, (-6))))) + M;
+    }
+    return M;
+}
