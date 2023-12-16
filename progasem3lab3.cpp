acfbad1bd479eec2230d919a2ada7bafec1a69b9
ElenaRUDN <126961213+ElenaRUDN@users.noreply.github.com>
2023-12-16 14:48:02 +0300

    #1

---
+++ progasem3lab3.cpp
@@ -0,0 +1,25 @@
+﻿
+#include <iostream>
+
+class Service {
+public:
+	std::string nameofService;
+	unsigned timeofService;
+	std::string nameofDog;
+	std::string is_it_a_difficult_dog;
+	std::string costofService;
+};
+
+class Sitting: public Service {
+	std::string need_a_walk;
+	std::string feedingTime;
+};
+
+class Walk : public Service {
+	std::string interaction_with_other_dogs;
+	std::string need_to_know;
+};
+
+class Company {
+
+};
